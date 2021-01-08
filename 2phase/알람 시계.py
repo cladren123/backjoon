3998b80e6071b1b10403132ab5617e9d1ed45f45
cladren <gold3987@naver.com>
2021-01-08 20:05:59 +0900

    if문

---
+++ 2phase/알람 시계.py
@@ -0,0 +1,12 @@
+hour, minute = map(int, input().split())
+
+if minute >= 45 :
+    minute = minute - 45;
+else :
+    minute = minute + 60 -45
+    if hour == 0 :
+        hour = 23;
+    else :
+        hour = hour -1
+
+print(hour, minute)
